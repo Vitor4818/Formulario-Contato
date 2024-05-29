5ae40b88c7cc779fea2637944081f6ad0f33d4ac
Vitor4818 <vitorg.martins4818@gmail.com>
2024-05-29 17:37:14 -0300

    WIP: Terminando validação de campos

--- js/script.js
+++ js/script.js
@@ -3,21 +3,67 @@
     const lastNameInput = document.getElementById('last-name')
     const firstNameError = document.getElementById('error-name')
     const lastNameError = document.getElementById('error-last-name')
+    const emailInput = document.getElementById('email')
+    const emailErro = document.getElementById('email-error')
+    const queryInput= document.getElementsByClassName('queryInput')
+    const queryError = document.getElementById('queryError')
+    const messageInput = document.getElementById('message')
+    const messageError = document.getElementById('textareaError')
     
 
     
     function validar(event){
         event.preventDefault()
+
+        let isValid = true;
+
         if (firstNameInput.value.trim() === '') {
             firstNameError.textContent = 'First Name is required';
+            isValid = false
     }else{
         firstNameError.textContent = ""
     }
+
+
         if (lastNameInput.value.trim() === '') {
             lastNameError.textContent = 'Last Name is required';
+            isValid = false
         }else{
             lastNameError.textContent = ""
         }
+
+
+
+    if (emailInput.value.trim() === ''){
+        emailErro.textContent = 'Email is required'
+        isValid = false
+    }else{
+        emailErro.textContent = ''
+    }
+    
+    let isRadioChecked = false;
+    for (let radio of queryInput){
+        if(radio.checked){
+            isRadioChecked = true;
+            break
+        }
+    }
+
+    if(!isRadioChecked){
+        queryError.textContent = 'Select an option'
+        isValid = false
+    }else{
+        queryError.textContent = ''
+    }
+
+    if(messageInput.value.trim() ===''){
+        messageError.textContent = 'This field is required'
+        isValid = false
+    }else{
+        messageError.textContent = ''
+    }
+
+
     } 
 
     form.addEventListener('submit', validar)
